3f1246ee8069bf09cab8d9f2e110e4a72f2c01aa
Karn Kaul <karnkaul@gmail.com>
2026-03-30 15:39:44 -0700

    example/square

---
+++ examples/square.cpp
@@ -0,0 +1,49 @@
+#include "clap/build_version.hpp"
+#include "clap/parameter.hpp"
+#include "clap/parser.hpp"
+#include "clap/program.hpp"
+#include <cstdlib>
+#include <print>
+
+namespace {
+[[nodiscard]] auto run(int const argc, char const* const* argv) {
+	auto const program_name = clap::to_program_name(*argv);
+	auto const program = clap::Program{
+		.name = program_name,
+		.version = clap::build_version_v,
+		.description = "print the square of an integer",
+	};
+	auto input = int{};
+	auto verbose = bool{};
+	auto parameter_list = std::vector<clap::Parameter>{
+		clap::named_flag(verbose, "v,verbose", "print full expression"),
+		clap::positional_required(input, "NUM", "integer to square"),
+	};
+
+	auto parser = clap::Parser{std::move(parameter_list), program};
+	auto const result = parser.parse_main(argc, argv);
+	if (result.should_early_exit()) { return result.return_code(); }
+
+	if (verbose) { std::print("{} x {} = ", input, input); }
+	auto const output = input * input;
+	std::println("{}", output);
+
+	return EXIT_SUCCESS;
+}
+} // namespace
+
+int main(int argc, char** argv) {
+	try {
+		if (argc < 1) {
+			std::println(stderr, "argc is < 1 ({})", argc);
+			return EXIT_FAILURE;
+		}
+		return run(argc, argv);
+	} catch (std::exception const& e) {
+		std::println(stderr, "PANIC: {}", e.what());
+		return EXIT_FAILURE;
+	} catch (...) {
+		std::println("PANIC!");
+		return EXIT_FAILURE;
+	}
+}
